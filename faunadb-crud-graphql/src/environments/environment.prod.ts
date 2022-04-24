dc51d3750b1ba2374f7425f4938a01dd8c465c58
gtaveras <gregorihh11@gmail.com>
2022-04-24 13:22:06 -0400

    Start of project with libraries

---
+++ faunadb-crud-graphql/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  FAUNADB_SERVER_SECRET: 'fnAEk9NRyGACQYIFncW9K5vf5Dwu8CoPx3Ce3b9h'
+};
---
+++ faunadb-crud-graphql/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  FAUNADB_SERVER_SECRET: 'fnAEk9NRyGACQYIFncW9K5vf5Dwu8CoPx3Ce3b9h'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
